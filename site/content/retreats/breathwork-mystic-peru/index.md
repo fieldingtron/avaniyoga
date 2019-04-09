69b7319bcf3f6dd55a1f7598873857d3e4d339e5
Fields Marshall <fields@nuspace.com>
2019-04-09 13:48:42 -0400

    contact me

---
+++ site/content/retreats/breathwork-mystic-peru/index.md
@@ -0,0 +1,86 @@
+---
+title: "Yoga, Meditation and Hiking"
+date: 2018-03-02T17:05:11-03:00
+dates: Oct, Nov, Dec 
+image: yogaAndMeditation
+draft: false
+resources:
+- src: "yogaAndMeditation1.jpg"
+  title: "Yummy Cupcake"
+  name: cupcake-1
+  params: 
+    credits: "Very Good Meditation"
+- src: "*2.jpg"
+  title: "Shiny Glaze"
+- src: "*.jpg"
+  params: 
+    credits: "Myself the cook!"
+---
+### Yoga, Meditation and Hiking Yoga Retreat
+
+This amazing retreat is a combination of body and mind and is designed to lift you up spiritually and bring you back to earth, surrounding yourself with wilderness, beauty, and finding your inner self. You may even find yourself on the crater of one of Chile's most active volcanoes, the Villarica volcano. From daily yoga routines and national parks to active volcanoes and hot springs, this yoga and hike retreat in Pucon, Chile is one of those life time experiences you just have to go for.
+
+Here is some new text/ 
+
+more text
+ mas text
+
+**RETREAT HIGHLIGHTS**
+
+* Daily yoga classes
+* Daily meditation sessions
+* Daily Activity: Most days this is Hiking
+* Organic juices and smoothies
+* 5 nights’ accommodation
+* Organic meals
+
+**YOGA STYLES**
+
+* Hatha
+* 5 days with instruction in English
+
+**Spoken languages**
+
+* English, Spanish
+
+### Accommodation
+
+The venue will be a beautiful eco-lodge set on an acre of abundant nature and gardens. It is located on the hill near Lake Villarica in northern Patagonia. The yoga lodge is located near a mountain stream and at night you can relax to the sound of running water and in the morning be woken up by birdsong. All buildings’ interiors are made of natural materials. You will stay in a twin or double room that is heated with radiators so that it will be warm and comfortable.
+
+### Avani Yoga Retreats Chile Yoga Retreat Schedule
+
+#### Day 1 Arrival
+
+* 1 yoga class
+* Welcome lunch
+
+#### Day 2 Huerquehue National Park
+
+* 1 yoga class
+* Full day hike
+* Optional kayaking, hot tub
+* Breakfast and lunch
+
+#### Day 3 Hot Springs Day
+
+* 1 yoga class
+* Hot springs
+* Breakfast and lunch
+
+#### Day 4 Cañi National Park
+
+* 1 yoga class
+* Full day hike
+* Breakfast and lunch
+
+#### DAY 5 SPA DAY & REST DAY
+
+* 1 yoga class
+* 1 full body 1-hour massage
+* Option to pick an additional add-on
+* Breakfast, lunch and farewell dinner
+
+#### DAY 6 FAREWELL
+
+* 1 yoga class
+* Breakfast
--- site/static/js/contactform.js
+++ site/static/js/contactform.js
@@ -1,31 +1,77 @@
-  function validateForm() {
-  var name =  document.getElementById('name').value;
-  if (name == "") {
-      document.getElementById('status').innerHTML = "Name cannot be empty";
-      return false;
-  }
-  var email =  document.getElementById('email').value;
-  if (email == "") {
-      document.getElementById('status').innerHTML = "Email cannot be empty";
-      return false;
-  } else {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if(!re.test(email)){
-          document.getElementById('status').innerHTML = "Email format invalid";
-          return false;
-      }
-  }
-  var subject =  document.getElementById('subject').value;
-  if (subject == "") {
-      document.getElementById('status').innerHTML = "Subject cannot be empty";
-      return false;
-  }
-  var message =  document.getElementById('message').value;
-  if (message == "") {
-      document.getElementById('status').innerHTML = "Message cannot be empty";
-      return false;
-  }
-  document.getElementById('status').innerHTML = "Sending...";
-  document.getElementById('contact-form').submit();
+/* eslint-disable no-undef */
+$(function() { $("#contactForm input,select,textarea").not("[type=submit]").jqBootstrapValidation({
+  preventSubmit: true,
+  submitError: function($form, event, errors) {
+    console.log("error");
 
-  }
+    // additional error messages or events
+  },
+  submitSuccess: function($form, event) {
+    console.log("submitting");
+    event.preventDefault(); // prevent default submit behaviour
+    // get values from FORM
+    var name = $("input#contact-name").val();
+    var email = $("input#contact-email").val();
+    var subject = $("input#contact-subject").val();
+    var message = $("textarea#contact-message").val();
+    var firstName = name; // For Success/Failure Message
+    // Check for white space in name for Success/Fail message
+    if (firstName.indexOf(" ") >= 0) {
+      firstName = name.split(" ").slice(0, -1).join(" ");
+    }
+    $this = $("#sendMessageButton");
+    $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+    $.ajax({
+      url: "https://jumprock.co/mail/gonkertron",
+      type: "POST",
+      data: {
+        name: name,
+        subject: subject,
+        email: email,
+        message: message
+      },
+      cache: false,
+      success: function() {
+        // Success message
+        $("#success").html("<div class='alert alert-success'>");
+        $("#success > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          .append("</button>");
+        $("#success > .alert-success")
+          .append("<strong>Your message has been sent. </strong>");
+        $("#success > .alert-success")
+          .append("</div>");
+        //clear all fields
+        $("#contactForm").trigger("reset");
+      },
+      error: function() {
+        // Fail message
+        $("#success").html("<div class='alert alert-danger'>");
+        $("#success > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          .append("</button>");
+        $("#success > .alert-danger").append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+        $("#success > .alert-danger").append("</div>");
+        //clear all fields
+        $("#contactForm").trigger("reset");
+      },
+      complete: function() {
+        setTimeout(function() {
+          $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+        }, 1000);
+      }
+    });
+  },
+  filter: function() {
+    return $(this).is(":visible");
+  },
+});
+
+$("a[data-toggle=\"tab\"]").click(function(e) {
+  e.preventDefault();
+  $(this).tab("show");
+});
+});
+
+/*When clicking on Full hide fail/success boxes */
+$("#name").focus(function() {
+  $("#success").html("");
+});
