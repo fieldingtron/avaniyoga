ac5644299cbea07588168a2d77022f92d37b26d3
Fields Marshall <fields@nuspace.com>
2019-04-22 21:06:09 -0400

    new endpoint

--- static/js/stripez.js
+++ static/js/stripez.js
@@ -28,7 +28,7 @@ const handler = StripeCheckout.configure({
   },
   token: function(token) {
 
-    fetch(`http://localhost:8888/.netlify/functions/stripe-charge/stripe-charge`, {
+    fetch(`https://mysticretreats.netlify.com/.netlify/functions/stripe-charge/stripe-charge`, {
       method: 'POST',
       body: JSON.stringify({
         token,
